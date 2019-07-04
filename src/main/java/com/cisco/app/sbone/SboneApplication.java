09836c184bb4895f037d1b4e80e84ccb1480916d
Sankar Murugesan <amsankar@gmail.com>
2019-07-04 11:07:34 +0530

    update

---
+++ src/main/java/com/cisco/app/sbone/SboneApplication.java
@@ -0,0 +1,13 @@
+package com.cisco.app.sbone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SboneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SboneApplication.class, args);
+	}
+
+}
